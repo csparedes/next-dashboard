b65a84adc2b77a792dcce07ef4e892d3d61a3bc3
Cristian Paredes <csparedes1995@gmail.com>
2024-12-14 20:45:31 -0500

    initialized repo for nextjs tutorial

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+
+import { jetbrains, inter } from "./ui/fonts";
+import "./ui/global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { JetBrains_Mono, Inter, Lusitana } from "next/font/google";
+
+export const jetbrains = JetBrains_Mono({ subsets: ["latin"] });
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
